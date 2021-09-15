31f84d420046e7b91666c43fa59ae526c7d1ba9f
daklo91 <coffeedog91@gmail.com>
2021-09-15 14:19:33 +0200

    Using props in a button onClick

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,16 @@
 function Todo(props) {
+  function deleteHandler() {
+    console.log("handler working");
+    console.log(props.text);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">DELETE</button>
+        <button className="btn" onClick={deleteHandler}>
+          DELETE
+        </button>
       </div>
     </div>
   );
