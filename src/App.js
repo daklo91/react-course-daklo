679bbe6e3cefc45c65b149439d4ef6dd091eef64
daklo91 <coffeedog91@gmail.com>
2021-09-15 14:03:44 +0200

    Add props.text from parent to child

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
-      <Todo />
-      <Todo />
+      <Todo text="Hi! I'm a prop!" />
+      <Todo text="I'm another prop" />
+      <Todo text="Luigi" />
     </div>
   );
 }
