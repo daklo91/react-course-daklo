0398605223e4a52181e3507881f2dd48e8091d69
daklo91 <coffeedog91@gmail.com>
2021-09-16 10:41:04 +0200

    Add closeModal on both delete and close

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,13 @@
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
       <p>Are you sure?</p>
-      <button className="btn btn--alt">Cancel</button>
-      <button className="btn">Delete</button>
+      <button className="btn btn--alt" onClick={props.closeModal}>
+        Cancel
+      </button>
+      <button className="btn" onClick={props.deleteTodo}>
+        Delete
+      </button>
     </div>
   );
 }
